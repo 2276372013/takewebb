96e03537f01c20da4e9c15b25c601002c4635f32
haoliang <2276372013@qq.com>
2021-03-25 10:42:45 +0800

    物品列表页ok

--- src/app/pages/test/test.component.ts
+++ src/app/pages/test/test.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EChartsOption } from 'echarts';
+import getISOWeek from 'date-fns/getISOWeek';
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
@@ -10,6 +11,16 @@ export class TestComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+  date: Date[] = [new Date(),new Date()];
+
+  onChange(result: Date[]): void {
+    console.log('onChange: ', result);
+    console.log(this.date)
   }
 
+  getWeek(result: Date[]): void {
+    console.log('week: ', result.map(getISOWeek));
+  }
 }
