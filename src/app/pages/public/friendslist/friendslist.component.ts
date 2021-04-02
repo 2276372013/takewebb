034de4e536cd2e52e62c74592ae9c75df1c43492
haoliang <2276372013@qq.com>
2021-04-02 16:52:44 +0800

    ad

--- src/app/pages/public/friendslist/friendslist.component.ts
+++ src/app/pages/public/friendslist/friendslist.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { User } from '../../../interfaceEntity/Entity/user.interface';
 import { FriendsService } from '../../../service/Friends.service';
 import { Msg } from '../../../interfaceEntity/Entity/Msg.interface';
@@ -10,19 +10,64 @@ import { Msg } from '../../../interfaceEntity/Entity/Msg.interface';
 })
 export class FriendslistComponent implements OnInit {
 
-  constructor(private friendsService: FriendsService) {}
+  constructor(private friendsService: FriendsService) { }
   offsetTop = 10;
   visible = false;
   childrenVisible = false;
   vegetables = ['asparagus', 'bamboo', 'potato', 'carrot', 'cilantro', 'potato', 'eggplant'];
-  friendsList:User[];
-  
+  friendsList: User[];
+  userDescribe = '';
+  userPhoto = '';
+  //EventEmitter 属性
+  voted = new EventEmitter<boolean>();
+  userName = '';
+  show = false;
+
   ngOnInit(): void {
     this.friendsService.findAllFriends().subscribe(
       (result: Msg) => {
         if ((result.status === 200)) {
-          this.friendsList = result.data;
-          console.log(this.friendsList)
+          this.friendsList = result.data.users;
+        } else {
+        }
+      }
+    );
+  }
+
+  Hello(e: MouseEvent, userId: String) {
+    console.log(userId);
+    this.findPhotoDescribe(userId);
+  }
+
+  findPhotoDescribe(userId: String) {
+    this.friendsService.findPhotoDescribe(userId).subscribe(
+      (result: Msg) => {
+        if ((result.status === 200)) {
+          this.userDescribe = result.data.userDescribe;
+          this.userPhoto = result.data.userPhoto;
+        } else {
+        }
+      }
+    );
+  }
+
+  findUser() {
+    this.friendsService.findUser(this.userName).subscribe(
+      (result: Msg) => {
+        if ((result.status === 200)) {
+          if (result.data) {
+            this.friendsService.findPhotoDescribe(result.data.userId).subscribe(
+              (result: Msg) => {
+                if ((result.status === 200)) {
+                  this.userPhoto = result.data.userPhoto;
+                  this.userDescribe = result.data.userDescribe;
+                  this.show = true;
+                  this.userName = '';
+                } else {
+                }
+              }
+            );
+          }
         } else {
         }
       }
--- src/app/service/Friends.service.ts
+++ src/app/service/Friends.service.ts
@@ -8,4 +8,10 @@ export class FriendsService {
   public findAllFriends(): Observable<any> {
     return this.httpClient.post('http://localhost:8080/takeit/friends/findAllFriends',null);
   }
+  public findPhotoDescribe(userId:String): Observable<any> {
+    return this.httpClient.post('http://localhost:8080/takeit/friends/findPhotoDescribe',userId);
+  }
+  public findUser(userName:String): Observable<any> {
+    return this.httpClient.post('http://localhost:8080/takeit/friends/findUser',userName);
+  }
 }
